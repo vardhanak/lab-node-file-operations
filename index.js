3e878ed8d60b54f48a283efd5c8b5c57cc5ba625
vardhanak <63716978+vardhanak@users.noreply.github.com>
2022-04-03 20:55:51 +0530

    commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const data = require("./data");
+const os =  require ("os");
+const fs = require ("fs");
+const crypto = require('crypto');
 
+var value = os.userInfo();
+var userdata= data("Prograd",2020,"BE")
+const password = crypto.createHmac('sha256',"secret").update('Prograd').digest('hex');
+
+fs.appendFileSync("message.txt", "ProGrad Details: \n")
+
+fs.appendFileSync("message.txt", `\tUserId ->        ${value.uid},\n`)
+fs.appendFileSync("message.txt", `\tName ->          ${userdata.name},\n`)
+fs.appendFileSync("message.txt", `\tYear ->          ${userdata.Year},\n`)
+fs.appendFileSync("message.txt", `\tQualification -> ${userdata.Qualification},\n`)
+fs.appendFileSync("message.txt", `\tUserName ->      ${value.uname},\n`)
+fs.appendFileSync("message.txt", `\tPassword ->      ${password},\n`)
+
+console.log(value.uid)
+console.log(userdata.name)
+console.log(userdata.Year)
+console.log(userdata.Qualification)
+console.log(value.username)
+console.log(password)
