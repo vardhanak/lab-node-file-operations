3a7879ebc9979187f51773ad37f5296f88cb14a9
vardhanak <63716978+vardhanak@users.noreply.github.com>
2022-04-03 20:55:09 +0530

    commit

--- data.js
+++ data.js
@@ -1,1 +1,10 @@
-
+function data(name,year,qualification)
+{
+    userData ={
+        name:name,
+        Year:year,
+        Qualification:qualification
+    }
+    return userData
+}
+module.exports = data
